dc868d651277cb962a8ad17aa662f4ffee935f5d
Deepak Nagaraj <n.deepak@gmail.com>
2016-01-28 12:18:06 -0800

    add golang paths

--- config.fish
+++ config.fish
@@ -3,9 +3,6 @@
 # fbsd specific settings
 set -x PACKAGESITE http://ftp-archive.freebsd.org/mirror/FreeBSD-Archive/old-releases/amd64/8.4-RELEASE/packages/Latest/
 
-# path
-set -U fish_user_paths {$HOME}/bin /usr/bin.real /usr/texbin
-
 # env
 set -x P4CONFIG .p4config
 set -x P4DIFF colordiff
@@ -15,6 +12,13 @@ set -x NSADDR 10.217.212.6
 set -x NSLOGIN nsroot@$NSADDR
 set -x P4PORT SJCPperforce04.citrite.net:1666
 set -x P4USER deepaknag
+
+# env: go
+set -x GOROOT ~/go
+set -x GOPATH ~/gowork
+
+# path
+set -U fish_user_paths ~/bin /usr/bin.real /usr/texbin $GOROOT/bin $GOPATH/bin
 
 # aliases
 alias post-review "post-review --server=https://reviewboard.citrite.net --p4-port=$P4PORT"
